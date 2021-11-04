5b06249f19dcf0211ec66c5cb94e8b5d038268c6
Remy <canaler@clarkson.edu>
2021-11-04 12:16:07 -0400

    Made images responive from help from prof York! decided to change score board format and now have a rough idea of how js is going to work

---
+++ JS/script.js
@@ -0,0 +1,9 @@
+//session storage will be useful
+
+//make sure there is no null using if statements
+
+//(1)display results
+
+//(2)remove images hide function
+
+//(3) Write values for score using session storage
